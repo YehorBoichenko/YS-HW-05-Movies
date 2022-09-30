0cbbedc40f51eee496216ac9af104af850d33e86
YehorBoichenko <99806345+YehorBoichenko@users.noreply.github.com>
2022-09-30 14:46:39 +0000

    Deploying to gh-pages from @ YehorBoichenko/YS-HW-05-Movies@7e6601a8dc8723e69ce8e64ce95061c4c20ad139 🚀

---
+++ static/js/998.7b5325d5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[998],{2998:function(e,t,n){n.r(t);n(2791);var u=n(184);t.default=function(){return(0,u.jsx)("h1",{children:"404 Page not found!"})}}}]);
+//# sourceMappingURL=998.7b5325d5.chunk.js.map
